4bae8525d362b1fb5d7549234103f59ac08f68ed
ahmed077 <don.zero23@gmail.com>
2017-11-07 18:16:55 +0200

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,24 +1,18 @@
 /*jslint plusplus:true*/
 /*global console*/
-var gradOne, gradTwo, drawBezire, addPoint, createRandomPoints,
+var gradOne, gradTwo, drawBezire, addPoint, createRandomPoints, animate, animateBackground, getMax,
     canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     w = canvas.clientWidth,
-    h = canvas.clientHeight;
+    h = canvas.clientHeight,
+    maxY = h;
 ctx.fillStyle = "#2b2d3c";
 ctx.fillRect(0, 0, w, h);
 gradOne = ctx.createLinearGradient(0, 0, 0, h);
 gradOne.addColorStop(0, "#2570b0");
 gradOne.addColorStop(1, "#2b2d3c");
 ctx.fillStyle = gradOne;
-ctx.fillRect(0, h / 2, w, h / 2);
-ctx.beginPath();
-ctx.moveTo(0, 100);
-ctx.lineTo(w, 100);
 ctx.strokeStyle = "#2196f3";
-ctx.stroke();
-ctx.fillStyle = 'red';
-
 var XHR = new XMLHttpRequest(),
     points;
 XHR.open('GET', './points.txt', true);
@@ -27,37 +21,80 @@ XHR.onreadystatechange = function () {
     'use strict';
     if (XHR.readyState === 4 && XHR.status === 200) {
         points = JSON.parse(XHR.responseText);
-        animate();
+        animate(points, 5000 / points.length);
     }
 };
 var i = 0,
-    coords = {x: 0, y: 0},
-    int;
-function animate() {
+    intstroke,
+    intfill;
+function animate(p, t) {
     'use strict';
-    ctx.save();
     ctx.strokeStyle = "#0ff";
     ctx.beginPath();
-    ctx.moveTo(0, 350);
-    int = setInterval(function () {
-        ctx.closePath();
-        ctx.lineTo(parseInt(points[i].x, 10), parseInt(points[i].y, 10));
-        ctx.moveTo(parseInt(points[i].x, 10), parseInt(points[i].y, 10));
+    ctx.moveTo(parseInt(p[0].x, 10), parseInt(p[0].y, 10));
+    intstroke = setInterval(function () {
+        ctx.lineWidth = 5;
+        ctx.lineTo(parseInt(p[i].x, 10), parseInt(p[i].y, 10));
+        ctx.lineJoin = 'round';
         ctx.stroke();
+        maxY = getMax(maxY, parseInt(p[i].y, 10));
         i++;
-        if (i > w) {
+        if (i === p.length) {
+            ctx.lineWidth = 0;
+            ctx.lineTo(w, h);
+            ctx.lineTo(0, h);
             ctx.closePath();
-            ctx.restore();
-            clearInterval(int);
+            animateBackground(gradOne);
+            clearInterval(intstroke);
         }
-    }, 5);
-    
+    }, t);
 }
+function getMax(num1, num2) {
+    'use strict';
+    return num1 < num2 ? num1 : num2;
+}
+function animateBackground(color) {
+    'use strict';
+//    ctx.save();
+    ctx.fillStyle = color;
+    ctx.clip();
+    console.log(maxY);
+    intfill = setInterval(function () {
+        ctx.fillRect(0, 0, w, maxY + 1);
+        maxY += 1;
+        if (maxY >= h) {
+            clearInterval(intfill);
+        }
+    }, 10);
+//    ctx.restore();
+}
+function drawBezire(sx, sy, cpx1, cpy1, cpx2, cpy2, ex, ey) {
+    'use strict';
+    ctx.save();
+//    addPoint(sx, sy);
+//    addPoint(cpx1, cpy1);
+//    addPoint(cpx2, cpy2);
+//    addPoint(ex, ey);
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(sx, sy);
+    ctx.bezierCurveTo(cpx1, cpy1, cpx2, cpy2, ex, ey);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
 function addPoint(x, y) {
     'use strict';
     ctx.closePath();
     ctx.save();
     ctx.ellipse(x, y, 5, 5, 0, 0, Math.PI * 2);
+//    ctx.font = '20px Arial';
+//    ctx.fillText("(" + x + ", " + y + ")", x, y - 10);
+//    ctx.strokeStyle = "#fff";
+    ctx.fill();
+//    ctx.strokeText("(" + x + ", " + y + ")", x, y - 10);
     ctx.closePath();
     ctx.restore();
 }
